97b6f1343f517c907f4dbd9c5f32bf252d0616cd
39345 <39345@MSI.homenet.telecomitalia.it>
2020-07-10 11:32:19 +0200

    //

--- EserciziJava/src/it/begear/esercizi/giorno7/esercizio3/Main.java
+++ EserciziJava/src/it/begear/esercizi/giorno7/esercizio3/Main.java
@@ -16,116 +16,114 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Animale[] acquarioPesci = new Animale[5];
-		Animale[] acquarioMammiferi = new Animale[5];
+		Animale[] acquarioPesci = new Animale[2];
+		Animale[] acquarioMammiferi = new Animale[2];
 
 		Scanner scannerUtente = new Scanner(System.in);
+		String continua = null;
+		do {
+			System.out.println("Benvenuto nell'acquario ufficiale della Miskatonic University ");
+			System.out.println("--------------------------------------------------------------");
+			System.out.println("-------inserisci la classe della creatura da creare-----------");
+			System.out.println("-------------------Mammifero o Pesce--------------------------");
+			String classe = scannerUtente.next();
+			System.out.println("---------inserisci il nome della creatura da creare-----------");
+			String nome = scannerUtente.next();
+			Animale animale1 = new Animale(nome.toLowerCase(), classe.toLowerCase());
+			System.out.println("------Abbiamo appena creato un " + classe + " di nome " + nome + " ------");
+			System.out.println("Abbiamo a disposizione due acquari, uno per pesci e uno per i mammiferi");
+			System.out.println("---vuoi mettere la creatura appena creata dentro uno di questi?--");
+			System.out.println("------------------------Yes/No-----------------------------------");
+			String decisione = scannerUtente.next();
+			if (decisione.toLowerCase().equals("yes") || decisione.toLowerCase().equals("y")) {
 
-		System.out.println("Benvenuto nell'acquario ufficiale della Miskatonic University ");
-		System.out.println("--------------------------------------------------------------");
-		System.out.println("-------inserisci la classe della creatura da creare-----------");
-		System.out.println("-------------------Mammifero o Pesce--------------------------");
-		String classe = scannerUtente.next();
-		System.out.println("---------inserisci il nome della creatura da creare-----------");
-		String nome = scannerUtente.next();
-		Animale animale1 = new Animale(nome.toLowerCase(), classe.toLowerCase());
-		System.out.println("------Abbiamo appena creato un " + classe + " di nome " + nome + " ------");
-		System.out.println("Abbiamo a disposizione due acquari, uno per pesci e uno per i mammiferi");
-		System.out.println("---vuoi mettere la creatura appena creata dentro uno di questi?--");
-		System.out.println("------------------------Yes/No-----------------------------------");
-		String decisione = scannerUtente.next();
-		if (decisione.toLowerCase().equals("yes") || decisione.toLowerCase().equals("y")) {
+				System.out.println("-----Per mettere l'animale nell'acquario di Mammiferi premi M---");
+				System.out.println("------Per mettere l'animale nell'acquario di Pesci premi P------");
+				String acquarioScelto = scannerUtente.next();
+				switch (acquarioScelto.toLowerCase()) {
+				case "M":
+				case "m":
+				case "mammifero":
+				case "Mammifero":
+					inserisciAnimale(acquarioMammiferi, animale1);
+					break;
 
-			System.out.println("-----Per mettere l'animale nell'acquario di Mammiferi premi M---");
-			System.out.println("------Per mettere l'animale nell'acquario di Pesci premi P------");
-			String acquarioScelto = scannerUtente.next();
-
-			if (acquarioScelto.toLowerCase().equals("m") && animale1.getClasse().equals("mammifero")) {
-				for (int i = 0; i < acquarioMammiferi.length; i++) {
-					if (acquarioMammiferi[i] == null) {
-						acquarioMammiferi[i] = animale1;
-						break;
-
-					}
-
-					else {
-						System.out.println("------Siamo spiacemti ma non c'è più spazio nell'acquario------");
-
-					}
-
-				}
-			} else if (acquarioScelto.toLowerCase().equals("p") && animale1.getClasse().toLowerCase().equals("pesce")) {
-				for (int i = 0; i < acquarioPesci.length; i++) {
-					if (acquarioPesci[i] == null) {
-						if (acquarioPesci[i] == null) {
-							acquarioPesci[i] = animale1;
-							break;
-
-						}
-					}
-
-					else {
-						System.out.println("------Siamo spiacemti ma non c'è più spazio nell'acquario------");
-
-					}
-
+				case "P":
+				case "p":
+				case "pesci":
+				case "Pesci":
+					inserisciAnimale(acquarioPesci, animale1);
+					break;
+				default:
+					System.out
+							.println("------Siamo spiacemti ma è pericoloso inserire mammiferi e pesci insieme------");
+					System.out
+							.println("--------------------------azione non consentita-------------------------------");
+					throw new IllegalArgumentException("Unexpected value: " + acquarioScelto.toLowerCase());
 				}
 
-			} else {
-				System.out.println("------Siamo spiacemti ma è pericoloso inserire mammiferi e pesci insieme------");
-				System.out.println("--------------------------azione non consentita-------------------------------");
+			} else if (decisione.toLowerCase().equals("no") || decisione.toLowerCase().equals("n")) {
+				System.out.println(
+						"----------------nessun problema, verrà messo in una vasca separata------------------");
 			}
-
-		} else if (decisione.toLowerCase().equals("no") || decisione.toLowerCase().equals("n")) {
-			System.out.println("----------------nessun problema, verrà messo in una vasca separata------------------");
-		}
-		System.out.println(
-				"----------------Vuoi visualizzare il contentuto delle due vasche principali?------------------");
-		System.out.println(
-				"-----------------------------------Yes/No------------------------------------------------------");
-		String decisione2 = scannerUtente.next();
-		if (decisione2.toLowerCase().contentEquals("y") || decisione2.toLowerCase().contentEquals("yes")) {
-			if (acquarioMammiferi[0] != null && acquarioMammiferi[1] != null) {
-				System.out.println("----------------ecco l'acquario di mammiferi------------------");
-				System.out.println(acquarioMammiferi[0].getNome() + " " + acquarioMammiferi[1].getNome());
-			}
-			if (acquarioMammiferi[0] != null && acquarioMammiferi[1] == null) {
-				System.out.println("----------------ecco l'acquario di mammiferi------------------");
-				System.out.println(acquarioMammiferi[0].getNome());
-			}
-			if (acquarioMammiferi[0] == null && acquarioMammiferi[1] != null) {
-				System.out.println("----------------ecco l'acquario di mammiferi------------------");
-				System.out.println(acquarioMammiferi[1].getNome());
-			}
-			if (acquarioMammiferi[0] == null && acquarioMammiferi[1] == null) {
-				System.out.println("----------------ecco l'acquario di mammiferi------------------");
-				System.out.println("-------spiacenti, è vuoto-------------------------------------");
-			}
-			if (acquarioPesci[0] != null && acquarioPesci[1] != null) {
-				System.out.println("----------------ecco l'acquario di pesci----------------------");
-				System.out.println(acquarioPesci[0].getNome() + " " + acquarioPesci[1].getNome());
-			}
-			if (acquarioPesci[0] != null && acquarioPesci[1] == null) {
-				System.out.println("----------------ecco l'acquario di pesci----------------------");
-				System.out.println(acquarioPesci[0].getNome());
-			}
-			if (acquarioPesci[0] == null && acquarioPesci[1] != null) {
-				System.out.println("----------------ecco l'acquario di pesci----------------------");
-				System.out.println(acquarioPesci[1].getNome());
-			}
-			if (acquarioPesci[0] == null && acquarioPesci[1] == null) {
-				System.out.println("----------------ecco l'acquario di pesci------------------");
-				System.out.println("-------spiacenti, è vuoto-------------------------------------");
-			}
-
-		} else if (decisione2.toLowerCase().contentEquals("n") || decisione2.toLowerCase().equals("no")) {
-
 			System.out.println(
-					"-----------------------------------Grazie, alla prossima creatura!---------------------------");
+					"----------------Vuoi visualizzare il contentuto delle due vasche principali?------------------");
+			System.out.println(
+					"-----------------------------------Yes/No------------------------------------------------------");
+			String decisione2 = scannerUtente.next();
+			if (decisione2.toLowerCase().contentEquals("y") || decisione2.toLowerCase().contentEquals("yes")) {
+
+				System.out.println("----------------ecco l'acquario di mammiferi------------------");
+				stampaAcquario(acquarioMammiferi);
+
+				System.out.println("----------------ecco l'acquario di pesci----------------------");
+				stampaAcquario(acquarioPesci);
+				// da qui creo il Sostitutore String sceltaElimino = scannerUtente.next();
+				// aggiungere la possibilità di far combattere le vasche generando mathrandom di attacco e difesa delle
+				//creature?
+
+			} else if (decisione2.toLowerCase().contentEquals("n") || decisione2.toLowerCase().equals("no")) {
+
+				System.out.println(
+						"-----------------------------------Grazie, alla prossima creatura!---------------------------");
+
+			}
+			System.out.println("-----------------------------------Vuoi continuare ?---------------------------");
+			System.out.println("-----------------------------------Yes / No---------------------------");
+			continua = scannerUtente.next();
+
+		} while (continua.toLowerCase().equals("y") || continua.toLowerCase().equals("yes"));
+		System.out.println("-----------------------------------Ottimo lavoro doc---------------------------");
+
+	}
+
+	private static void inserisciAnimale(Animale[] acquario, Animale animale) {
+		for (int i = 0; i < acquario.length; i++) {
+
+			if (acquario[i] == null) {
+				acquario[i] = animale;
+				break;
+
+			}
+
+			else {
+				System.out.println("------Siamo spiacemti ma non c'è più spazio nell'acquario------");
+
+			}
 
 		}
+	}
 
-		System.out.println("-----------------------------------Ottimo lavoro doc---------------------------");
+	private static void stampaAcquario(Animale[] acquario) {
+		for (Animale animale : acquario) {
+			if (animale != null) {
+				System.out.println(animale.getNome());
+			}
+		}
+	}
+
+	private static void rimuoviAnimale(Animale[] acquario, Animale animale) {
+
 	}
 
 }
